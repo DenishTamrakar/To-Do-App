c6e41fbc8b1971d74c208e6afda57c94c3248222
Denish Tamrakar <tmkrdenish@gmail.com>
2025-01-01 17:19:47 +0545

    Started a program and added Models

---
+++ ToDo/Data/AppDBContext.cs
@@ -0,0 +1,4 @@
+
+public class AppDBContext : DbContext{
+    
+}
---
+++ ToDo/Models/ToDo.cs
@@ -0,0 +1,6 @@
+public class ToDo{
+    public int TDID{get; set;}
+    public string? TDName{get; set;}
+    public bool TDStatus{get; set;}
+    public int UserID{get; set;}
+}
